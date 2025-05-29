c2d882ca79f87a4eb5960a57b0620d837c54446d
Ash Christopher <ash.christopher@shopify.com>
2025-05-28 20:09:25 -0400

    Updates the code to use latest FastMCP

--- src/pagerduty_mcp_server/__init__.py
+++ src/pagerduty_mcp_server/__init__.py
@@ -2,7 +2,26 @@
 
 A server that exposes PagerDuty API functionality to LLMs. This server is designed to be used programmatically, with structured inputs and outputs.
 """
+import logging
+from .client import get_api_client
+from .server import mcp
 
-from .server import server
+__all__ = ["main", "mcp"]
 
-__all__ = ['server']
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+# )
+logger = logging.getLogger("pagerduty_mcp_server")
+
+
+def main():
+    """Main entry point for the package."""
+    try:        
+        get_api_client()
+    except SystemExit:
+        return 1
+    mcp.run(transport="stdio")
+
+if __name__ == "__main__":
+    main()
